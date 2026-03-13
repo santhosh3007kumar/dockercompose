1e6eee5d1dc1dde4699ecd9d072c9727c57fc2c1
santhosh3007kumar <kumarsanthosh18905@gmail.com>
2026-03-13 17:12:22 +0530

    2st commit

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,9 @@ const express = require("express");
 const app = express();
 
 app.get("/api", (req,res)=>{
- res.send("Hello from Backend API 🚀");
+ res.send("Hello from Backend 🚀");
 });
 
-app.listen(5000, ()=>{
- console.log("Backend running on port 5000");
+app.listen(5000,"0.0.0.0",()=>{
+ console.log("Backend running");
 });
